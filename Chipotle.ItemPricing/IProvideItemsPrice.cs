48dc350e212c3f2c776a99b9cf0ebc97fa7806eb
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-02-13 18:09:00 +0530

    Update Chipotle Pricing

---
+++ Chipotle.ItemPricing/IProvideItemsPrice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chipotle.ItemPricing
+{
+  public  class IProvideItemsPrice
+    {
+      public List<ItemPrice>  getAllIemsPrice(int id)
+      {
+          ISeedItemsPrice db = new ISeedItemsPrice();
+          return  db.ItemPrices.Where(i=>i.Id == id).ToList();
+      }
+      
+    }
+}
---
+++ Chipotle.ItemPricing/ISeedItemsPrice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chipotle.ItemPricing
+{
+    public class ISeedItemsPrice : DbContext
+    {
+        public IDbSet<ItemPrice> ItemPrices { get; set; }
+    }
+
+    public class MyItenPricingInitializer : DropCreateDatabaseIfModelChanges<ISeedItemsPrice>
+    {
+        protected override void Seed(ISeedItemsPrice context)
+        {
+            context.ItemPrices.Add(new ItemPrice { Name = "BARRITO", Price = 250.89 });
+            context.ItemPrices.Add(new ItemPrice { Name = "BARRITO BOWl", Price = 350.59 });
+            context.SaveChanges();
+
+        }
+    }
+}
